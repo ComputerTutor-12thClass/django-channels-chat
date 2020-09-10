888f7bab70d7a503740593c1df316f2dbe8ecbfe
Santosh Kumar <shkr2209@mail.com>
2020-09-10 10:03:36 +0530

    lcommit

--- chat/urls.py
+++ chat/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include('core.urls')),
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
-     
+
     path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'},
          name='logout')
 
